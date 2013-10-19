fa7cc1e5e9791989c7287a7378603954847dd2ce
Pedro Candeias <pgscandeias@gmail.com>
2013-10-19 20:07:29 +0100

    Dev fix

--- frontend/web/app.php
+++ frontend/web/app.php
@@ -8,17 +8,19 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 // Use APC for autoloading to improve performance.
 // Change 'sf2' to a unique prefix in order to prevent cache key conflicts
 // with other applications also using APC.
-/*
-$loader = new ApcClassLoader('sf2', $loader);
-$loader->register(true);
-*/
+
+// $loader = new ApcClassLoader('sf2', $loader);
+// $loader->register(true);
+
+$env = getenv('APP_ENV') ?: 'prod';
+$debug = (bool) getenv('APP_DEBUG');
 
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('prod', false);
+$kernel = new AppKernel($env, $debug);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+$kernel = new AppCache($kernel);
 Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
